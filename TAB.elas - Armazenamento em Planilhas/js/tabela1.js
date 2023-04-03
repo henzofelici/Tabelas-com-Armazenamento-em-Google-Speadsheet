3ae7e1529b22a4a0f2ab006edf8671727a71c2a2
h1n <henzo09gabriel05@gmail.com>
2023-04-03 10:25:37 -0300

    Add files via upload

---
+++ TAB.elas - Armazenamento em Planilhas/js/tabela1.js
@@ -0,0 +1,42 @@
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click", function () {
+    toggleDarkMode();
+    localStorage.removeItem("dark");
+    modeText.innerText = "Tema Claro";
+
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+        modeText.innerText = "Tema Escuro";
+    }
+});
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode == 1) {
+        toggleDarkMode();
+        modeText.innerText = "Tema Escuro";
+    } else {
+        modeText.innerText = "Tema Claro";
+    }
+}
+
+loadTheme();
